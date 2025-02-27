f5bb183dc94aac5e954a6cdfe584b21d0bf307c9
greysonfang <greysonfang@tencent.com>
2025-02-27 20:45:19 +0800

    feat:【PAC模板】流水线模板支持PAC特性 #11414

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/template/v2/PipelineTemplateInstanceFacadeService.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/template/v2/PipelineTemplateInstanceFacadeService.kt
@@ -1,8 +1,25 @@
 package com.tencent.devops.process.service.template.v2
 
+import com.tencent.devops.common.client.Client
+import com.tencent.devops.process.permission.PipelinePermissionService
+import com.tencent.devops.process.permission.template.PipelineTemplatePermissionService
+import org.jooq.DSLContext
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class PipelineTemplateInstanceFacadeService {
+class PipelineTemplateInstanceFacadeService @Autowired constructor(
+    private val pipelineTemplateCommonService: PipelineTemplateCommonService,
+    private val pipelineTemplateInfoService: PipelineTemplateInfoService,
+    private val pipelineTemplateModelParser: PipelineTemplateModelParser,
+    private val pipelineTemplatePACService: PipelineTemplatePACService,
+    private val pipelineTemplatePermissionService: PipelineTemplatePermissionService,
+    private val pipelinePermissionService: PipelinePermissionService,
+    private val pipelineTemplateResourceService: PipelineTemplateResourceService,
+    private val pipelineTemplateSettingService: PipelineTemplateSettingService,
+    private val dslContext: DSLContext,
+    private val client: Client,
+    private val pipelineTemplateRelatedService: PipelineTemplateRelatedService
+) {
 
 }
