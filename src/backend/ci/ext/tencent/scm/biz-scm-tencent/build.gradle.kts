b065c9e7f3b7953f6d6e2e8af1d698179641cb22
stubenhuang <stubenhuang@tencent.com>
2025-04-02 15:05:44 +0800

    feat: 升级JDK17 #10593 修复SCM部署失败问题

--- src/backend/ci/ext/tencent/scm/biz-scm-tencent/build.gradle.kts
+++ src/backend/ci/ext/tencent/scm/biz-scm-tencent/build.gradle.kts
@@ -26,10 +26,11 @@
  */
 
 dependencies {
-    api(project(":core:store:api-store")){
-        exclude(group = "org.springframework.cloud", module = "spring-cloud-starter-stream-rabbit")
-    }
+    api(project(":core:store:api-store"))
     api(project(":ext:tencent:scm:api-scm-tencent"))
     api(project(":ext:tencent:misc:api-monitoring-tencent"))
     api(project(":ext:tencent:common:common-digest-tencent"))
+    configurations.forEach {
+        it.exclude("org.springframework.cloud", "spring-cloud-starter-stream-rabbit")
+    }
 }
